1e8fa7368f484a5588c89b36f4457bafa39777b2
Lucas Bortolo <lucasbortolo3@gmail.com>
2025-03-04 12:39:25 -0300

    initial commit

---
+++ src/main/java/br/com/fiap/tech/sub_tech_challenge_management_api_fase5/SubTechChallengeManagementApiFase5Application.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tech.sub_tech_challenge_management_api_fase5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubTechChallengeManagementApiFase5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubTechChallengeManagementApiFase5Application.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fiap/tech/sub_tech_challenge_management_api_fase5/SubTechChallengeManagementApiFase5ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tech.sub_tech_challenge_management_api_fase5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubTechChallengeManagementApiFase5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
